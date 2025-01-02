f9941551776aa56741f6ae3fd192a71e15cd895a
Ernests Dejus <ernisd621@gmail.com>
2025-01-02 21:51:22 +0200

    ama rockstar popstar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import "./App.css"
-export default function MainPage(){
-    return(
-        <>
-        <nav>
-            
-        <div className='center-nav'>
-            <li><button>Home</button></li>
-            <li><button>Projects</button></li>
-            <li><button>About Me</button></li>
-        </div>
-         <div className='right-nav'> 
-            <li><button>Contact me</button></li>
-         </div>
-        </nav>
-        </>
-    )
-}
+import React from 'react'
+import './App.css'
 
+export default function MainPage() {
+  return (
+    
+    <nav className="nav-container">
+
+      {/* Center Pill */}
+      <div className="center-pill">
+        <div className="pill">
+          <button>Home</button>
+          <button>My Projects</button>
+          <button>About Me</button>
+        </div>
+      </div>
+
+      {/* Right Pill */}
+      <div className="right-pill">
+        <div className="pill">
+          <button>Contact Me</button>
+        </div>
+      </div>
+    </nav>
+  )
+}
