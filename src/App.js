eaae418e446daee2d0443d1f6d78fccb902b5cbd
Ernests Dejus <ernisd621@gmail.com>
2025-01-03 16:07:03 +0200

    begining of something big

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 export default function MainPage() {
   return (
-    
     <nav className="nav-container">
-
-      {/* Center Pill */}
       <div className="center-pill">
         <div className="pill">
-          <button>Home</button>
-          <button>My Projects</button>
-          <button>About Me</button>
+          <button>
+            <span className="material-icons icon">home</span>
+            Home
+          </button>
+          <button>
+            <span className="material-icons icon">work</span>
+            My Projects
+          </button>
+          <button>
+            <span className="material-icons icon">person</span>
+            About Me
+          </button>
         </div>
       </div>
 
-      {/* Right Pill */}
       <div className="right-pill">
         <div className="pill">
-          <button>Contact Me</button>
+          <button>
+            <span className="material-icons icon">mail</span>
+            Contact Me
+          </button>
         </div>
       </div>
     </nav>
-  )
+  );
 }
